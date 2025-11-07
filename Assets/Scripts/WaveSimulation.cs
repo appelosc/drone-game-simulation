7efa6b977cccf092979326e5aed182d9ce3d3c8a
appelosc <appelosc@arcada.fi>
2025-11-07 12:00:56 +0200

    Våglängd simulation tillagd

--- Assets/Scripts/WaveSimulation.cs
+++ Assets/Scripts/WaveSimulation.cs
@@ -1,16 +1,40 @@
+using System;
 using UnityEngine;
 
 public class WaveSimulation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Mesh mesh;
+    Vector3[] vertices;
+    Vector3[] originalVertices;
+
+    public float Amplitude;
+    public float Frequency;
+    public float Wavelength;
+
+
     void Start()
     {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            Vector3 v = vertices[i];
+            float x = v.x + v.z; 
+
+            
+            float y = Amplitude * Mathf.Sin(2 * Mathf.PI / Wavelength * x - 2 * Mathf.PI * Frequency * Time.time);
+
+            v.y = y;
+            vertices[i] = v;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
         
     }
 }
