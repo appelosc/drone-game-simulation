21d742f1110d2cdbe0416d553c02221e2e9ebcb9
appelosc <appelosc@arcada.fi>
2025-11-18 23:10:30 +0200

    drone controller angles and thurstforce added

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class DroneController : MonoBehaviour
@@ -6,6 +7,10 @@ public class DroneController : MonoBehaviour
     float F;
 
     float FloatingConst;
+
+    float rotationSpeed = 3f;
+
+    float thrust_force;
 
     Rigidbody Drone;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,38 +23,66 @@ public class DroneController : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
+    {  
+        float angle = Vector3.Angle(transform.up, Vector3.up);
+        float thrust_force = (Drone.mass * FloatingConst) / Mathf.Cos(angle * Mathf.Deg2Rad);
+        Drone.AddForce(transform.up * thrust_force);
 
         if(Input.anyKey == false)
         {
-            Drone.AddForce(Vector3.up * FloatingConst);
+            Quaternion targetRotation = Quaternion.Euler(0, 0, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+
         }
+
         if (Input.GetKey(KeyCode.W) == true)
         {
             
             Drone.AddForce(Vector3.up * F);
         }
+
         if (Input.GetKey(KeyCode.S) == true)
         {
            
             Drone.AddForce(Vector3.down * F);
-        }if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            transform.Rotate( new Vector3(0.3f, 0, 0) );           
-            Drone.AddForce(0,0,F);
+    
         }
+
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            Quaternion targetRotation = Quaternion.Euler(30f, Drone.rotation.eulerAngles.y, Drone.rotation.eulerAngles.z);
+            Quaternion newRotation = Quaternion.Lerp(Drone.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            Drone.MoveRotation(newRotation);
+            
+            Drone.AddForce(Vector3.forward * F);
+        }
+
         if (Input.GetKey(KeyCode.DownArrow) == true)
         {
-            Drone.AddForce(0,0,-F);
-
+            Quaternion targetRotation = Quaternion.Euler(-30f, Drone.rotation.eulerAngles.y, Drone.rotation.eulerAngles.z);
+            Quaternion newRotation = Quaternion.Lerp(Drone.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            Drone.MoveRotation(newRotation);
+            
+            Drone.AddForce(Vector3.back * F);
+            
         }
+
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {
-            Drone.AddForce(F,0,0);
+            Quaternion targetRotation = Quaternion.Euler(Drone.rotation.eulerAngles.x, Drone.rotation.eulerAngles.y, -30f);
+            Quaternion newRotation = Quaternion.Lerp(Drone.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            Drone.MoveRotation(newRotation);
+            
+            Drone.AddForce(Vector3.right * F);
 
-        }if (Input.GetKey(KeyCode.LeftArrow) == true)
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
-            Drone.AddForce(-F,0,0);
+            Quaternion targetRotation = Quaternion.Euler(Drone.rotation.eulerAngles.x, Drone.rotation.eulerAngles.y, 30f);
+            Quaternion newRotation = Quaternion.Lerp(Drone.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            Drone.MoveRotation(newRotation);
+            Drone.AddForce(Vector3.left * F);
 
         }
         
